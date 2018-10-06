c19abb48a2debca5d52af2abd839b8915bf8eadd
sujeet <harekrishna.bhumca13@gmai98l.com>
2018-10-06 17:31:56 +0530

    B_Kart_Projrct

---
+++ B_Kart_KKK/src/main/java/com/bkart/BKartKkkApplication.java
@@ -0,0 +1,12 @@
+package com.bkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BKartKkkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BKartKkkApplication.class, args);
+	}
+}
