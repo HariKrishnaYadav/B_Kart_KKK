6dfc96b6acc56be96d9372641d8bb464fa15556d
sujeet <harekrishna.bhumca13@gmai98l.com>
2018-11-24 11:21:07 +0530

    add category

---
+++ B_Kart_KKK_Category_Model/src/main/java/com/bkart/BKartKkkCategoryModelApplication.java
@@ -0,0 +1,12 @@
+package com.bkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BKartKkkCategoryModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BKartKkkCategoryModelApplication.class, args);
+	}
+}
